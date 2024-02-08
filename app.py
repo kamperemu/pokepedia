34cbdc58b30861bd5050ad19938255cafe25ca34
Vedang Patel <pvedang388@gmail.com>
2024-02-09 00:32:31 +0530

    feat: add routes

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/pokelist")
+def pokelist():
+    return render_template("pokelist.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
